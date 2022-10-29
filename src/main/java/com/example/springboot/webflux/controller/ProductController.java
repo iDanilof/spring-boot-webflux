6a76e9ca98367adc5a1b9cda50237f06364314b1
iDanilof <ivandaniel.vimos@gmail.com>
2022-10-29 20:24:15 +0200

    Add "form" endpoints to Controller to add a new Product.

--- src/main/java/com/example/springboot/webflux/controller/ProductController.java
+++ src/main/java/com/example/springboot/webflux/controller/ProductController.java
@@ -2,13 +2,16 @@ package com.example.springboot.webflux.controller;
 
 import java.time.Duration;
 
+import com.example.springboot.webflux.models.documents.Product;
 import com.example.springboot.webflux.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+import reactor.core.publisher.Mono;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,7 +21,7 @@ public class ProductController {
   private final ProductService productService;
   
   @GetMapping({"/list", "/"})
-  public String list(Model model) {
+  public Mono<String> list(Model model) {
     
     final var products = productService.findAllNamesUppercase();
     
@@ -27,7 +30,22 @@ public class ProductController {
     model.addAttribute("products", products);
     model.addAttribute("title", "List of Products");
     
-    return "list";
+    return Mono.just("list");
+  }
+  
+  @GetMapping("/form")
+  public Mono<String> create(Model model) {
+    model.addAttribute("product", new Product());
+    model.addAttribute("title", "Product form");
+    return Mono.just("form");
+  }
+  
+  @PostMapping("/form")
+  public Mono<String> save(Product product) {
+    return productService.save(product).doOnNext(product1 -> 
+        log.info("Product saved: " + product1.getName() + "Id: " + product1.getId()))
+        //.then(Mono.just("redirect:/list)) We can use it too
+        .thenReturn("redirect:/list");
   }
 
   @GetMapping("/list-data-driver")
